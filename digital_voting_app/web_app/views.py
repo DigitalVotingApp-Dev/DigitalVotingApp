d4fc6e12eb76e53112a9701148c9137887f2710c
bj96 <baljeetsingh1274@gmail.com>
2019-03-23 01:06:21 +0530

    Adding voter registration template view
    This page has to be modified in accordance to our requirements. (vote.html) in templates.

--- digital_voting_app/web_app/views.py
+++ digital_voting_app/web_app/views.py
@@ -4,7 +4,7 @@ from web_app.models import Voter
 
 def index(request):
     #return HttpResponse("<h1>HELLO</h1>")
-    return render(request, 'web_app/home.html')
+    return render(request, 'web_app/vote.html')
 
 def create_voter(params):
     voter = Voter(name = params['name'], age = params['age'], gender = params['gender'], email_id = params['email_id'], password = params['password'], aadhar_num = params['aadhar_num'], contact_num = params['contact_num'], father_name = params['father_name'], mother_name = params['mother_name'], permanent_address = params['permanent_address'], photograph_image_link = params['photograph_image_link'], signature_image_link = params['signature_image_link'], aadhar_doc_link = params['aadhar_doc_link'], occupation = params['occupation'], date_of_birth = params['date_of_birth'])
