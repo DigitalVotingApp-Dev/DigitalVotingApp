9bbc8ea45d68978d38da5e0ccbd63bf9f6c9081a
Ashutosh Rahi <ashutoshrahi1997@gmail.com>
2019-04-07 22:15:49 +0530

    Adding face recognition while login

--- digital_voting_app/digital_voting_app/settings.py
+++ digital_voting_app/digital_voting_app/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap_datepicker_plus',
+    #'bootstrap_datepicker_plus',
 ]
 
 # Use BOOTSTRAP3 if you are using Bootstrap 3
