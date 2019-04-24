20fb66c02eebec1367ce0b74e96d7ea0d3350042
Ashutosh Rahi <ashutoshrahi1997@gmail.com>
2019-04-24 23:21:07 +0530

    Removing unwanted changes

--- Final Project/FingerPrint/Extraction/binarize.m
+++
@@ -1,5 +0,0 @@
-imshow(I)
-figure
-J=I(:,:,1)>160;
-imshow(J)
-set(gcf,'position',[1 1 600 600]);
--- Final Project/FingerPrint/enhancement.m
+++
@@ -1,6 +0,0 @@
-I = imread('101_1.tif');
-figure,imshow(I)
-I1 = rgb2gray(I);
-figure,imshow(I1)
-gA=imadjust(I1,[.01 .6],[0 1]);
-figure, imshow(gA);
